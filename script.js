a06d9d117da06d2670fbfb5a2515029569b0ac63
Michael Shyu <michaelshyu11@gmail.com>
2023-11-13 21:28:13 -0500

    added cute dog with js and html

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ const text1 = document.getElementById("text1");
 
 board.addEventListener('click', function() {
     const head1Visibility = window.getComputedStyle(head1).visibility;
-    console.log("clocked");
     if(head1Visibility == "hidden") {
         head1.style.visibility = "visible";
         text1.style.visibility = "hidden"; 
@@ -20,12 +19,25 @@ const text2 = document.getElementById("text2");
 
 board2.addEventListener('click', function() {
     const head2Visibility = window.getComputedStyle(head2).visibility;
-    console.log("clocked");
     if(head2Visibility == "hidden") {
         head2.style.visibility = "visible";
         text2.style.visibility = "hidden"; 
     } else {
         head2.style.visibility = "hidden";
         text2.style.visibility = "visible";
+    }
+});
+
+const board3 = document.getElementById("board3");
+const head3 = document.getElementById("header3");
+const img = document.querySelector("img");
+board3.addEventListener('click', function() {
+    const head3Visibility = window.getComputedStyle(head3).visibility;
+    if(head3Visibility == "hidden") {
+        head3.style.visibility = "visible";
+        img.style.visibility = "hidden"; 
+    } else {
+        head3.style.visibility = "hidden";
+        img.style.visibility = "visible";
     }
 });
