181d025a448178c430d626148e9b8955541f61d2
Michael Shyu <michaelshyu11@gmail.com>
2023-11-13 21:16:12 -0500

    adding elements to html and style as well as some js

--- script.js
+++ script.js
@@ -1,4 +1,31 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+const board = document.getElementById("board");
+const head1 = document.getElementById("header1");
+const text1 = document.getElementById("text1");
 
-document.querySelector("body").appendChild(h2);
+board.addEventListener('click', function() {
+    const head1Visibility = window.getComputedStyle(head1).visibility;
+    console.log("clocked");
+    if(head1Visibility == "hidden") {
+        head1.style.visibility = "visible";
+        text1.style.visibility = "hidden"; 
+    } else {
+        head1.style.visibility = "hidden";
+        text1.style.visibility = "visible";
+    }
+});
+
+const board2 = document.getElementById("board2");
+const head2 = document.getElementById("header2");
+const text2 = document.getElementById("text2");
+
+board2.addEventListener('click', function() {
+    const head2Visibility = window.getComputedStyle(head2).visibility;
+    console.log("clocked");
+    if(head2Visibility == "hidden") {
+        head2.style.visibility = "visible";
+        text2.style.visibility = "hidden"; 
+    } else {
+        head2.style.visibility = "hidden";
+        text2.style.visibility = "visible";
+    }
+});
